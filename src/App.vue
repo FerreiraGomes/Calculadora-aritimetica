d51c6e77d0e1ac9b4afe227a1e570f9e6321b50a
gabriel <gabrielpcnet53@gmail.com>
2024-01-24 20:26:20 -0300

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4">
+      <h1>Calculadora Arítimetica</h1>
+      <p>Você poderá fazer várias contas utilizando essa calculadora</p>
+    </header>
+    <form class="text-center">
+      <div class="row">
+        <div class="col-md-1">
+          <input type="number" class="form-control" />
+        </div>
+        <div class="col-md-1">
+          <select class="form-control text-center">
+            <option value="adicao">+</option>
+            <option value="subitracao">-</option>
+            <option value="multiplicacao">x</option>
+            <option value="divisao">%</option>
+          </select>
+        </div>
+        <div class="col-md-1">
+          <input type="number" class="form-control" />
+        </div>
+        <div class="row">
+          <div class="col-md-3 mt-3">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
